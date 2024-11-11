88cf8365a925237e111ccdbf8e443ea0468ffe37
Emilio del Hierro <delhierroemilio@gmail.com>
2024-11-11 16:02:50 -0500

    version final

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ document.addEventListener('astro:page-load',() => {
     console.log("Index Page loaded");
     // index.js
     function updateUserLink() {
+        
+
         const user = JSON.parse(localStorage.getItem('currentUser'));
         const userElement = document.getElementById('user');
         const userElementMobile = document.getElementById('user-mobile');
         if (user) {
-            userElement.innerHTML = `<a href="/profile">Hello, ${user.name}</a>`;
+            userElement.innerHTML = `<button id="user-name">Hello, ${user.name}</button>
+                    <div id="div-submenu" class="hidden flex-col  absolute text-white bg-indigo-700 w-full mt-4 border-2 border-white p-2 gap-4">
+                        <a href="/profile" class="text-center">Profile</a>
+                        <button id="logout">Log Out</button>
+                    </div>`;
             userElementMobile.innerHTML = `<a href="/profile">Hello, ${user.name}</a>`;
         } else {
             userElement.innerHTML = `<a href="/login">Login</a>`;
@@ -17,7 +23,7 @@ document.addEventListener('astro:page-load',() => {
 
     
         function checkReload() {
-            if (document.location.pathname === '/newflat' || document.location.pathname === '/allflats') {
+            if (document.location.pathname === '/newflat' || document.location.pathname === '/allflats' || document.location.pathname === '/recovery' || document.location.pathname === '/' || document.location.pathname === '/profile') {
               // Verifica si la página ya se ha refrescado
               if (!localStorage.getItem('pageRefreshed')) {
                   // Si no se ha refrescado, refresca la página
@@ -41,5 +47,21 @@ document.addEventListener('astro:page-load',() => {
         }
     }
     showMenu()
+
+    const anchor = document.querySelector('#user-name');
+    
+    const div = document.querySelector('#div-submenu');
+
+    anchor.addEventListener('click', () => {
+        div.classList.toggle('hidden');
+        div.classList.toggle('flex');
+    });
+    
+    const logout = document.querySelector('#logout');
+    logout.addEventListener('click', () => {
+        localStorage.removeItem('currentUser');
+        window.location.href = '/';
+    });
+    
 
 })
